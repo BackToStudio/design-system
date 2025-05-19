b5d67425d678153d27d22dca7d279326af53f772
Maïté Armengaud <53378428+maitearmengaud@users.noreply.github.com>
2025-05-19 16:57:36 +0200

    feat: add decorator for image

---
+++ src/Image/FloatAlignDecorator.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace PlusQuePro\DesignSystem\Image;
+
+use PlusQuePro\DesignSystem\StyleDecorator;
+
+class FloatAlignDecorator implements StyleDecorator
+{
+    private string $textAlign = '';
+
+    public function __construct(string $textAlign)
+    {
+        $this->textAlign = $textAlign;
+    }
+
+    public function getClassName(): string
+    {
+        $textAlignClasses = [
+            'left' => 'float-left mr-auto',
+            'right' => 'float-right ml-auto',
+        ];
+
+        $className = '';
+
+        if (
+            !empty($this->textAlign) &&
+            array_key_exists($this->textAlign, $textAlignClasses)
+        ) {
+            $className = $textAlignClasses[$this->textAlign];
+        }
+
+        return $className;
+    }
+}
