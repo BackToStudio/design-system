169c4ba4f6bf27e3e28ea4c2ab2b368519f54985
Maïté Armengaud <53378428+maitearmengaud@users.noreply.github.com>
2025-05-21 18:05:19 +0200

    refactor: supprimer une classe CSS obsolète dans PostFeaturedImageDecorator

--- src/Decorators/PostFeaturedImageDecorator.php
+++ src/Decorators/PostFeaturedImageDecorator.php
@@ -7,7 +7,6 @@ class PostFeaturedImageDecorator implements StyleDecorator
     public function getClassName(): string
     {
         $className = [];
-        $className[] = '[&_img]:w-full';
         $className[] = '[&_img]:rounded-media';
         $className[] = '[&_img]:object-cover';
 
