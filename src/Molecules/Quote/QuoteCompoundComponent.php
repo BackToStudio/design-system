8cb707dbd5b7313fe5d3886541b9b2b0a2750ecf
Maïté Armengaud <53378428+maitearmengaud@users.noreply.github.com>
2025-05-21 14:30:54 +0200

    feat: ajouter le composant QuoteCompoundComponent avec gestion des citations et du contenu

---
+++ src/Molecules/Quote/QuoteCompoundComponent.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace PlusQuePro\DesignSystem\Molecules\Quote;
+
+use PlusQuePro\DesignSystem\Atoms\TokenComponentWithChildren;
+use PlusQuePro\DesignSystem\Molecules\MoleculeInterface;
+
+class QuoteCompoundComponent extends TokenComponentWithChildren implements MoleculeInterface
+{
+    private TokenComponentWithChildren $citeComponent;
+    private TokenComponentWithChildren $contentComponent;
+
+    public function __construct()
+    {
+        $this->citeComponent = new TokenComponentWithChildren();
+        $this->contentComponent = new TokenComponentWithChildren();
+    }
+
+    public function getContentComponent(): TokenComponentWithChildren
+    {
+        return $this->contentComponent;
+    }
+
+    public function getCiteComponent(): TokenComponentWithChildren
+    {
+        return $this->citeComponent;
+    }
+
+    public function getMarkup(): string
+    {
+        $this->setTagName('blockquote');
+        $this->addClass('wp-block-quote');
+
+        $this->citeComponent->setTagName('cite');
+        $this->contentComponent->addClass('wp-block-quote__content');
+
+        $this->addChild($this->contentComponent->getMarkup());
+        $this->addChild($this->citeComponent->getMarkup());
+
+        return parent::getMarkup();
+    }
+}
