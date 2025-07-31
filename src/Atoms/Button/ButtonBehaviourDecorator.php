363e089706d5793beadb4bb252a5e6642de2bc84
Maïté Armengaud <53378428+maitearmengaud@users.noreply.github.com>
2025-07-31 12:15:55 +0200

    refactor: modify ButtonBehaviourDecorator to include space

--- src/Atoms/Button/ButtonBehaviourDecorator.php
+++ src/Atoms/Button/ButtonBehaviourDecorator.php
@@ -8,6 +8,6 @@ class ButtonBehaviourDecorator implements StyleDecorator
 {
     public function getClassName(): string
     {
-        return 'w-autoborder-2 inline-flex items-center gap-2xs py-xs px-s rounded-full';
+        return 'w-auto border-2 inline-flex items-center gap-2xs py-xs px-s rounded-full';
     }
 }
