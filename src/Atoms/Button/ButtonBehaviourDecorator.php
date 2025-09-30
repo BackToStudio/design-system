45438d1a14582f89af3778dc0b8c9b8b06f5868d
Maïté Armengaud <53378428+maitearmengaud@users.noreply.github.com>
2025-09-30 15:51:33 +0200

    refactor: update ButtonBehaviourDecorator to include 'leading-none' class for improved button styling

--- src/Atoms/Button/ButtonBehaviourDecorator.php
+++ src/Atoms/Button/ButtonBehaviourDecorator.php
@@ -8,6 +8,6 @@ class ButtonBehaviourDecorator implements StyleDecorator
 {
     public function getClassName(): string
     {
-        return 'w-auto border-2 inline-flex items-center gap-2xs py-xs px-s rounded-full';
+        return 'w-auto border-2 inline-flex items-center gap-2xs py-xs px-s rounded-full leading-none';
     }
 }
