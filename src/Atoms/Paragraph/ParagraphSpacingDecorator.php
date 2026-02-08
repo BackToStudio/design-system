7e8671615174b0dee3840acfcb23334e140a3270
Maïté Armengaud <53378428+maitearmengaud@users.noreply.github.com>
2026-02-08 17:06:14 +0100

    refactor: update spacing classes in ParagraphSpacingDecorator for improved styling consistency

--- src/Atoms/Paragraph/ParagraphSpacingDecorator.php
+++ src/Atoms/Paragraph/ParagraphSpacingDecorator.php
@@ -9,8 +9,8 @@ class ParagraphSpacingDecorator implements StyleDecorator
     public function getClassName(): string
     {
         $className = [];
-        $className[] = '[&_+_*]:mt-xs';
-        $className[] = '[*_+_&]:mt-xs';
+        $className[] = '[&_+_*]:mt-2xs';
+        $className[] = '[*_+_&]:mt-2xs';
         $className[] = '[&_+_.wp-block-group]:mt-s';
         $className[] = '[&_+_.wp-block-group]:md:mt-m';
         $className[] = '[&.text-caption_+_*]:!mt-xs';
